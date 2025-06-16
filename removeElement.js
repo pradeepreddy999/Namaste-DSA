ea5102f5a26ad4849c80fe2a5e44b5104ca33e37
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-06-16 20:19:21 +0530

    add problem to removeElement file.

--- removeElement.js
+++ removeElement.js
@@ -1,3 +1,13 @@
+// Given an integer array nums and an integer val,
+// remove all occurrences of val in-place. The order of the elements may be changed.
+// Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k. To get accepted, you need to:
+
+// Modify nums such that the first k elements contain elements not equal to val.
+// The remaining elements beyond k do not matter.
+// Return k.
+
 const removeElement = (nums, val) => {
   let k = 0;
   for (let i = 0; i < nums.length; i++) {
