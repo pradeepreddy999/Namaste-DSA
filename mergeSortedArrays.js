be8ec124ca96d527b8c276ad1464aa56a61f54f3
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-06-17 18:24:54 +0530

    add another approach for merge sorted array problem.

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,4 +1,5 @@
 // brute force approach
+// time complexity O((m+n)log(m+n)) and space complexity O(1)
 const mergeSortedArrays = (nums1, m, nums2, n) => {
   for (let i = m; i < m + n; i++) {
     nums1[i] = nums2[i - m];
@@ -10,6 +11,7 @@ const mergeSortedArrays = (nums1, m, nums2, n) => {
 // console.log(mergeSortedArrays([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
 // console.log(mergeSortedArrays([1], 1, [], 0));
 
+// time complexity O(m+n) and space complexity O(m) (nums1 is being copied)
 const mergeSortedArrays2 = (nums1, m, nums2, n) => {
   const nums1Copy = nums1.slice(0, m);
   let p1 = 0;
@@ -22,6 +24,28 @@ const mergeSortedArrays2 = (nums1, m, nums2, n) => {
     } else {
       nums1[i] = nums1Copy[p1];
       p1++;
+    }
+  }
+  return nums1;
+};
+
+// console.log(mergeSortedArrays([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+// console.log(mergeSortedArrays([1], 1, [], 0));
+
+// time complexity O(m+n) and space complexity O(1)
+const mergeSortedArrays3 = (nums1, m, nums2, n) => {
+  let p1 = m - 1;
+  let p2 = n - 1;
+
+  for (let i = m + n - 1; i > -1; i--) {
+    if (p2 < 0) break;
+
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1];
+      p1--;
+    } else {
+      nums1[i] = nums2[p2];
+      p2--;
     }
   }
   return nums1;
