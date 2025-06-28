454a3b39a4b12188151b14954cf84d653219ce0e
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-06-28 21:51:07 +0530

    add linked list cycle better approach (Floyd's algorithm also knows as Tortoise and Hare algorithm).

--- Linked List/linkedListCycle.js
+++ Linked List/linkedListCycle.js
@@ -1,6 +1,7 @@
 // Given head, the head of a linked list,
 // determine if the linked list has a cycle in it.
 
+// time complexity O(n) and space complexity O(n)
 var hasCycle = function (head) {
   let curr = head;
   let hash = new Set();
@@ -8,6 +9,28 @@ var hasCycle = function (head) {
     if (hash.has(curr)) return true;
     hash.add(curr);
     curr = curr.next;
+  }
+  return false;
+};
+
+// better approach - floyd's algorithm (slow pointer and fast pointer will
+// definitely meet at a point may be in the future if there is a cycle)
+
+// Floyd’s Cycle Detection (Tortoise and Hare algorithm)
+// slow and fast pointer approach
+
+// time complexity O(n) and space complexity O(1)
+var hasCycle2 = function (head) {
+  if (!head) return false;
+
+  let slow = head,
+    fast = head.next;
+
+  while (fast && fast.next) {
+    if (slow === fast) return true;
+
+    slow = slow.next;
+    fast = fast.next.next;
   }
   return false;
 };
