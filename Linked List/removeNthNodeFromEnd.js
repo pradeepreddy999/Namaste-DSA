93ae33ad0d10f41841f16a03b7ea205e3c90e604
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-06-30 11:28:14 +0530

    add a better approach that happens in one pass for removing nth node from end in a linked list problem.

--- Linked List/removeNthNodeFromEnd.js
+++ Linked List/removeNthNodeFromEnd.js
@@ -1,6 +1,7 @@
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
 // time complexity O(n) and space complexity O(1)
+// happens in two pass
 const removeNthFromEnd = (head, n) => {
   let sentinel = new ListNode();
   sentinel.next = head;
@@ -17,5 +18,32 @@ const removeNthFromEnd = (head, n) => {
   for (let i = 0; i < prevPos; i++) prev = prev.next;
 
   prev.next = prev.next.next;
+  return sentinel.next;
+};
+
+// better approach
+// two pointer approach
+
+// time complexity O(n) and space complexity O(1)
+// still this happens in one pass unlike above which happens in two pass.
+const removeNthFromEnd2 = (head, n) => {
+  // add a sentinel (dummy) node at start
+  let sentinel = new ListNode();
+  sentinel.next = head;
+
+  let second = sentinel;
+  let first = sentinel;
+  // move the first pointer n steps ahead
+  for (let i = 0; i < n; i++) first = first.next;
+
+  // move both pointers until first reaches last node
+  while (first.next) {
+    second = second.next;
+    first = first.next;
+  }
+
+  //delete node next to second
+  second.next = second.next.next;
+
   return sentinel.next;
 };
