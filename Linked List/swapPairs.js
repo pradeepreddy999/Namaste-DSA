94123b1ad79c52ab53337c263436e08c7a1abb89
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-06-30 18:53:04 +0530

    add recursive approach to the swapping two adjacent pairs of linked list problem.

--- Linked List/swapPairs.js
+++ Linked List/swapPairs.js
@@ -28,3 +28,35 @@ const swapPairs = (head) => {
 
   return dummy.next;
 };
+
+const swapPairs2 = (head) => {
+  if (!head || !head.next) return head;
+
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+
+  while (prev.next && prev.next.next) {
+    let first = prev.next;
+    let second = first.next;
+
+    first.next = second.next;
+    second.next = first;
+    prev.next = second;
+
+    prev = first;
+  }
+  return dummy.next;
+};
+
+// recursive approach
+const swapPairs3 = (head) => {
+  if (!head || !head.next) return head;
+
+  let left = head;
+  let right = head.next;
+
+  left.next = swapPairs3(right.next);
+  right.next = left;
+  return right;
+};
