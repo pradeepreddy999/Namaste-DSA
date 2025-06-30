7bda75abe664a099761d89e14696c265f76d31af
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-06-30 15:30:49 +0530

    add adding two linked list and returning a new Linked list problem.

---
+++ Linked List/addTwoLists.js
@@ -0,0 +1,23 @@
+// You are given two non-empty linked lists representing two non-negative integers.
+// The digits are stored in reverse order, and each of their nodes contains a single digit.
+// Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero,
+// except the number 0 itself.
+
+const addTwoNumbers = (l1, l2) => {
+  let dummy = new ListNode(0);
+  let curr = dummy,
+    carry = 0;
+
+  while (l1 || l2 || carry) {
+    let sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    l1 = l1?.next;
+    l2 = l2?.next;
+  }
+
+  return dummy.next;
+};
