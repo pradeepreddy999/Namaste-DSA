45e667e2a274b7767f4f592db2b3718ad4e4a848
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-07-01 22:46:04 +0530

    add finding length of last word of a string problem.

---
+++ Strings/lengthOfLastWord.js
@@ -0,0 +1,34 @@
+// time complexity O(n) and space complexity O(n)
+const lengthOfLastWord = (s) => s.trim().split(" ")?.at(-1).length;
+
+// time complexity O(n) and space complexity O(1)
+const lengthOfLastWord2 = (s) => {
+  let i = s.length - 1;
+
+  while (i > -1 && s[i] === " ") i--;
+
+  let count = 0;
+  while (i > -1 && s[i] !== " ") {
+    count++;
+    i--;
+  }
+
+  return count;
+};
+
+// time complexity O(n) and space complexity O(1) - better approach
+const lengthOfLastWord3 = (s) => {
+  let i = s.length - 1;
+
+  let count = 0;
+  while (i > -1) {
+    if (count > 0 && s[i] === " ") break;
+    else if (s[i] === " ") i--;
+    else {
+      count++;
+      i--;
+    }
+  }
+
+  return count;
+};
