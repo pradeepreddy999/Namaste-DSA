b7107dd8349ecddf80b59e1ee3627fd30a031cc0
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-07-02 21:08:56 +0530

    add finding words containing a given character problem.

---
+++ Strings/wordsContainingCharacter.js
@@ -0,0 +1,24 @@
+// You are given a 0-indexed array of strings words and a character x.
+// Return an array of indices representing the words that contain the character x.
+// Note that the returned array may be in any order.
+
+// m - no of words
+// n - max length of a word
+// Time Complexity O(m*n)
+// Space Complexity O(1)
+
+const findWordsContaining = (words, x) => {
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    // if(words[i].includes(x)) result.push(i);
+
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] === x) {
+        result.push(i);
+        break;
+      }
+    }
+  }
+
+  return result;
+};
