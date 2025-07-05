a4e67b2b291b9e2b5f87fa5c94aad010f093d222
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-07-05 23:40:52 +0530

    add finding largest odd number substring in a string problem.

---
+++ Strings/largestOddNumber.js
@@ -0,0 +1,15 @@
+// You are given a string num, representing a large integer.
+// Return the largest-valued odd integer (as a string)
+// that is a non-empty substring of num,
+// or an empty string "" if no odd integer exists.
+
+// A substring is a contiguous sequence of characters within a string.
+
+const largestOddNumber = (num) => {
+  let i = num.length - 1;
+  while (i >= 0) {
+    if (parseInt(num[i]) % 2 === 1) break;
+    i--;
+  }
+  return num.slice(0, i + 1);
+};
