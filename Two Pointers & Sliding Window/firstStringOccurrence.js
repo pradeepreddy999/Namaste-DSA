16370e46cd53a3278eea16a43d6e8230d840d9c0
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-07-22 19:52:11 +0530

    add first string occurrence problem using Knuth Morris Pratt algorithm.

--- Two Pointers & Sliding Window/firstStringOccurrence.js
+++ Two Pointers & Sliding Window/firstStringOccurrence.js
@@ -2,6 +2,7 @@
 // return the index of the first occurrence of needle in haystack,
 // or -1 if needle is not part of haystack.
 
+// time complexity O(n * m) can be O(n * n) in worst case when n === m, space complexity O(1)
 const strStr = (haystack, needle) => {
   for (let i = 0; i < haystack.length - needle.length + 1; i++) {
     let noOfMatches = 0;
@@ -9,6 +10,48 @@ const strStr = (haystack, needle) => {
       if (haystack[i + j] === needle[j]) noOfMatches++;
     }
     if (noOfMatches === needle.length) return i;
+  }
+  return -1;
+};
+
+// time complexity O(n), space complexity O(m) can be useful only when has common prefix and suffix
+
+const strStr2 = (haystack, needle) => {
+  let n = haystack.length;
+  let m = needle.length;
+  let lps = [0];
+  let i = 0;
+  let j = 1;
+  while (j < m) {
+    if (needle[i] === needle[j]) {
+      lps[j] = i + 1;
+      ++i;
+      ++j;
+    } else {
+      if (i == 0) {
+        lps[j] = 0;
+        ++j;
+      } else {
+        i = lps[i - 1];
+      }
+    }
+  }
+
+  i = j = 0;
+  while (i < n) {
+    if (haystack[i] === needle[j]) {
+      ++i;
+      ++j;
+    } else {
+      if (j == 0) {
+        ++i;
+      } else {
+        j = lps[j - 1];
+      }
+    }
+    if (j === m) {
+      return i - m;
+    }
   }
   return -1;
 };
